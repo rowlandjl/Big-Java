5045afa033b547fa153fe8b752ac78a8425c1561
rowlandjl <rowlandjl@vcu.edu>
2018-01-14 14:51:16 -0500

    add mysteryNum.java and stringExp.java

---
+++ Chapter-2/mysteryNum.java
@@ -0,0 +1,10 @@
+public class mysteryNum
+{
+  public static void main(String[] args)
+  {
+    int mystery = 1;
+    mystery = 1 - 2 * mystery;
+    mystery = mystery + 1;
+    System.out.println(mystery);
+  }
+}
