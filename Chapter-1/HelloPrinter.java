8d8b67c033ac835a5f9f95de647c7e623b8aa2e1
rowlandjl <rowlandjl@vcu.edu>
2018-01-14 13:52:06 -0500

    add chapter 1 and 2 exercises'

---
+++ Chapter-1/HelloPrinter.java
@@ -0,0 +1,9 @@
+public class HelloPrinter
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello");
+    System.out.println("");
+    System.out.println("World");
+  }
+}
---
+++ Chapter-2/Cans.java
@@ -0,0 +1,10 @@
+public class Cans
+{
+  public static void main(String[] args)
+  {
+    int cansPerPack = 6;
+    System.out.println(cansPerPack);
+    int cansPerCrate = 4 * cansPerPack;
+    System.out.println(cansPerCrate);
+  }
+}
